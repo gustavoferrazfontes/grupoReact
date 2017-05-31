2720140e838153e63616de7aa7a0e998f9741384
Gustavo Fontes <gustavo.ferrazfontes@gmail.com>
2017-05-31 00:04:41 -0300

    sugestao de mudanca de bind no construtor. issue #2

--- src/App.js
+++ src/App.js
@@ -2,39 +2,36 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      listTodo:[
-        {id:1,isComplete:true,text:'lavar o cachorro'},
-        {id:2,isComplete:false,text:'passear com o cachorro'},
-        {id:3,isComplete:true,text:'limpar merda do cachorro'},
-        {id:4,isComplete:false,text:'dar o cachorro'},
+      listTodo: [
+        { id: 1, isComplete: true, text: 'lavar o cachorro' },
+        { id: 2, isComplete: false, text: 'passear com o cachorro' },
+        { id: 3, isComplete: true, text: 'limpar merda do cachorro' },
+        { id: 4, isComplete: false, text: 'dar o cachorro' },
       ]
     };
 
-    this.notificarMudar = this.notificarMudar.bind(this); 
   }
 
-  notificarMudar(){
+  notificarMudar = (e) => e.preventDefault();
 
-  }
-
-  render(){
+  render() {
     return <div>
-    <header><h1>ToDo App</h1></header>
-    <fieldset>
-      <legend>Insira uma tarefa</legend>
-      <form>
-        <input type="text" />
-        <input type="submit" />
-      </form>
-    </fieldset>
-    <ul>
-      {this.state.listTodo.map(function(todo){
-        return <Linhazinha key={todo.id} todo={todo} mudou={() => this.notificarMudar()} />
-      }.bind(this))}
-    </ul>
+      <header><h1>ToDo App</h1></header>
+      <fieldset>
+        <legend>Insira uma tarefa</legend>
+        <form>
+          <input type="text" />
+          <input type="submit" />
+        </form>
+      </fieldset>
+      <ul>
+        {this.state.listTodo.map((todo) => {
+          return <Linhazinha key={todo.id} todo={todo} mudou={() => this.notificarMudar} />
+        })}
+      </ul>
     </div>
   }
 }
@@ -42,14 +39,14 @@ class App extends Component {
 export default App;
 
 class Linhazinha extends Component {
-  checkboxHandler(event) {
+  checkboxHandler = (e) => {
     // TODO: fazer direito. Usar uma classe para TODO e um método para alterar valor'
-    this.props.todo.isComplete = !this.props.todo.isComplete; 
+    this.props.todo.isComplete = !this.props.todo.isComplete;
     this.props.mudou();
   }
 
   render() {
     // onchange={this.checkboxHandler.bind(this,todo)}
-    return <li><input type="checkbox" checked={this.props.todo.isComplete} onChange={this.checkboxHandler.bind(this)} /> {this.props.todo.text}</li> 
+    return <li><input type="checkbox" checked={this.props.todo.isComplete} onChange={this.checkboxHandler} /> {this.props.todo.text}</li>
   }
 }  
